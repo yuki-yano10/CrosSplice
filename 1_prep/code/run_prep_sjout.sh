68448d090573f3a6bdb236a9abf09048cd11b3db
Yuki Yano <146058945+yuki-yano10@users.noreply.github.com>
2025-10-23 15:22:44 +0900

    Update run_prep_sjout.sh

--- 1_prep/code/run_prep_sjout.sh
+++ 1_prep/code/run_prep_sjout.sh
@@ -3,10 +3,11 @@
 set -euxo pipefail
 
 
-# This script generates a list of SJ.out.tab files. Make sure that you have already downloaded metadata.  
+1) Make a list of all SJ.out.tab file paths (sjouttab_file_path.txt).
+2) With this script, generate a list of all samples comtaining sample ID, RUN ID and tissue names. Make sure that you have already downloaded metadata.  
 
 SJOUTTAB_DIR=$WDIR/SJ_out_tab                              
 METADATA=$WDIR/refdata/SraRunTable.txt
 OUTPUT_FILE=$SJOUTTAB_DIR/sjouttab_list.txt
 
-python3 1_prep/make_gtex_sjouttab_list.py -path_to_sjouttab $SJOUTTAB_DIR/sjouttab_file.txt -metadata ${METADATA} -output_file ${OUTPUT_FILE}
+python3 1_prep/make_gtex_sjouttab_list.py -path_to_sjouttab $SJOUTTAB_DIR/sjouttab_file_path.txt -metadata ${METADATA} -output_file ${OUTPUT_FILE}
