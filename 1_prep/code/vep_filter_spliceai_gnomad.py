c2d615f16f7d3fd7847984b778bee34bcb6e4b79
Yuki Yano <146058945+yuki-yano10@users.noreply.github.com>
2025-10-23 15:39:04 +0900

    Update vep_filter_spliceai_gnomad.py

--- 1_prep/code/vep_filter_spliceai_gnomad.py
+++ 1_prep/code/vep_filter_spliceai_gnomad.py
@@ -1,19 +1,18 @@
-#!/usr/local/package/python/3.6.5/bin/python
-#$ -S /usr/local/package/python/3.6.5/bin/python
-
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
     
 import gzip
 
-chr_list="1, 2, 3, 4,5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, X"
-output_file = "/home/yano_y/GTEX_validation_project_V8/post_filter/GTEx_Analysis_2017-06-05_v8_WholeGenomeSeq_866Indiv.all.gnomad001.spliceaiG01.txt"
+working_directory = "/path/to/myproject" 
+output_file = working_directory + "/post_filter/input.all.gnomad001.spliceaiG01.txt"
 
 with open(output_file, "w") as hout:
     header = "Mut_key\tChr\tPos\tRef\tAlt\tGene\tSpliceAI_pred_DP_AG\tSpliceAI_pred_DP_AL\tSpliceAI_pred_DP_DG\tSpliceAI_pred_DP_DL\tSpliceAI_pred_DS_AG\tSpliceAI_pred_DS_AL\tSpliceAI_pred_DS_DG\tSpliceAI_pred_DS_DL\tSpliceAI_pred_SYMBOL\tgnomADg_AF\n"
     hout.write(header)
     
     for i in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,"X"]:
-        input_vcf = "/home/yano_y/GTEX_validation_project/post_vep/GTEx_Analysis_2017-06-05_v8_WholeGenomeSeq_866Indiv.chr"+str(i)+".sorted.rare-variant.vep.vcf.gz"
-        output_vcf = "/home/yano_y/GTEX_validation_project/post_filter/GTEx_Analysis_2016-01-15_v7_WholeGenomeSeq_652Ind_GATK_HaplotypeCaller."+ str(i) +".lift38.gnomad001.spliceaiG01.vcf"
+        input_vcf = working_directory + "/post_vep/input."+str(i)+".lift38.rare-variant.vep.vcf.gz"
+        output_vcf = working_directory + "/post_filter/input."+ str(i) +".lift38.gnomad001.spliceaiG01.vcf"
         
         with gzip.open(input_vcf, 'rt') as hin, open(output_vcf,"w") as vout:
            for line in hin:
