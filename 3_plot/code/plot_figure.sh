7f4e77e1da28ab4cab342017c05082e4f2d7ed9d
Yuki Yano <146058945+yuki-yano10@users.noreply.github.com>
2025-10-23 16:10:23 +0900

    Update plot_figure.sh

--- 3_plot/code/plot_figure.sh
+++ 3_plot/code/plot_figure.sh
@@ -5,12 +5,11 @@ PLOT_CODE=$3
 # calc p-value
 # 最後のオプションはプロット出力フラグ
 mkdir -p ${PLOT_DIR}/tsv
-/usr/local/package/r/4.3.2/bin/Rscript /home/yano_y/GTEX_validation_project/plot_code_virtual/pararell_get_pvalue_spliceai.R ${INPUT} ${PLOT_DIR} ${PLOT_CODE}
+Rscript ./code/pararell_get_pvalue_spliceai.R ${INPUT} ${PLOT_DIR} ${PLOT_CODE}
 
-# combine
-#rm -rf ${PLOT_DIR}/combined
+# combine p-values
 mkdir -p ${PLOT_DIR}/combined
-python /home/yano_y/GTEX_validation_project/plot_code/gather_combined_p.py ${PLOT_DIR}/tsv/ ${PLOT_DIR}/combined/gtex_validation_combined_p.tsv
+python ./code/gather_combined_p.py ${PLOT_DIR}/tsv/ ${PLOT_DIR}/combined/crossplice_validation_combined_p.tsv
 
 # plot (各プロジェクトで用意したスクリプトを使用)
 #Rscript /path/to/plot.R ${PLOT_DIR}/combined/gtex_validation_combined_p.tsv ${PLOT_DIR}/combined/plot_gtex_validation_combined.pdf
