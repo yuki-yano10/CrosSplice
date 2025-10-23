9909ecc278e826d294dee242dfcbdcde36bab82a
Yuki Yano <146058945+yuki-yano10@users.noreply.github.com>
2025-10-23 15:12:24 +0900

    Update run_prep_sjout.sh

--- 1_prep/code/run_prep_sjout.sh
+++ 1_prep/code/run_prep_sjout.sh
@@ -2,16 +2,11 @@
 #$ -S /usr/bin/bash
 set -euxo pipefail
 
-# 1) download GTEx sj_out_tab
 
-# 2) "Download Metadata:" from https://0-www-ncbi-nlm-nih-gov.brum.beds.ac.uk/Traces/study/?acc=PRJNA75899
+# This script generates a list of SJ.out.tab files. Make sure that you have already downloaded metadata.  
 
-# 3) make_gtex_sjouttab_list
-#GTEX_SJOUTTAB_DIR=/home/naiida/GTEx/data           # 1)
-#METADATA=./data/SraRunTable.txt                    # 2)
+SJOUTTAB_DIR=$WDIR/SJ_out_tab                              
+METADATA=$WDIR/refdata/SraRunTable.txt
+OUTPUT_FILE=$SJOUTTAB_DIR/sjouttab_list.txt
 
-
-METADATA=/home/yano_y/refdata/SraRunTable.txt
-OUTPUT_FILE=/home/yano_y/GTEx_sjout_scality/GTEx_sjouttab_list.txt
-
-python3 /home/yano_y/GTEX_validation_project/val_code/make_gtex_sjouttab_list.py -path_to_sjouttab /home/yano_y/GTEx_sjout_scality/GTEx_sjouttab_file.txt -metadata ${METADATA} -output_file ${OUTPUT_FILE}
+python3 1_prep/make_gtex_sjouttab_list.py -path_to_sjouttab $SJOUTTAB_DIR/sjouttab_file.txt -metadata ${METADATA} -output_file ${OUTPUT_FILE}
