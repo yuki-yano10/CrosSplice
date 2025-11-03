3a19a95d78c280b2daf6b6c0652321d6c132a166
Yuki Yano <146058945+yuki-yano10@users.noreply.github.com>
2025-11-03 23:46:34 +0900

    Update proc_vcf.sh

--- 1_prep/proc_vcf.sh
+++ 1_prep/proc_vcf.sh
@@ -17,17 +17,16 @@ mkdir -p ${POST_DIR}
 
 for i in ${CHR_LIST}; do
     CHR_NUM=${i}
-    CHR_VCF38_PRE=${VCF_DIR}/${VCF_STEM}.${CHR_NUM}.vcf
+    CHR_VCF38_PRE=${VCF_DIR}/${VCF_STEM}.${CHR_NUM}.vcf.gz
     CHR_VCF38_UNSORT=${VCF_DIR}/${VCF_STEM}.${CHR_NUM}.added.vcf
     CHR_VCF38_SORT=${VCF_DIR}/${VCF_STEM}.${CHR_NUM}.added.sorted.vcf
     CHR_VCF38=${POST_DIR}/${VCF_STEM}.${CHR_NUM}.added.sorted.vcf.gz
     
     # separate into chr and bgzip.
     bcftools-1.18/bcftools view -r ${CHR_NUM} -O v -o ${CHR_VCF38_PRE} ${INPUT_VCF38}
-    bgzip  -f -c ${CHR_VCF38_PRE} > ${CHR_VCF38_PRE}.gz
 
     # add "chr" prefix.
-    1_prep/add_chr.py -vcf ${CHR_VCF38_PRE}.gz -output ${CHR_VCF38_UNSORT}
+    1_prep/add_chr.py -vcf ${CHR_VCF38_PRE} -output ${CHR_VCF38_UNSORT}
 
     # sort and compress
     bcftools-1.18/bcftools sort ${CHR_VCF38_UNSORT} > ${CHR_VCF38_SORT}
