6a206b4894309302569a2835cf7fd95dde5661ee
Yuki Yano <146058945+yuki-yano10@users.noreply.github.com>
2025-11-04 00:00:41 +0900

    Update proc_vcf_liftover.sh

--- 1_prep/proc_vcf_liftover.sh
+++ 1_prep/proc_vcf_liftover.sh
@@ -2,7 +2,7 @@
 #$ -S /bin/bash
 set -euxo pipefail
 
-CHR_LIST="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 X Y" 
+CHR_LIST="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y" 
 WDIR="$1"
 INPUT_VCF37="$2"
 CHAIN="$3"
@@ -17,27 +17,22 @@ mkdir -p ${POST_DIR}
 
 for i in ${CHR_LIST}; do
     CHR_NUM=${i}
-    CHR_VCF37_PRE=${VCF_DIR}/${VCF_STEM}.${CHR_NUM}.vcf
+    CHR_VCF37_PRE=${VCF_DIR}/${VCF_STEM}.${CHR_NUM}.vcf.gz
     CHR_VCF38_POST=${VCF_DIR}/${VCF_STEM}.${CHR_NUM}.lift38.vcf
     CHR_VCF38_UNSORT=${VCF_DIR}/${VCF_STEM}.${CHR_NUM}.lift38.unsorted.vcf
-    CHR_VCF38_SORT=${VCF_DIR}/${VCF_STEM}.${CHR_NUM}.lift38.sorted.vcf
     CHR_VCF38=${POST_DIR}/${VCF_STEM}.${CHR_NUM}.lift38.sorted.vcf.gz
 
     # separate into chr.
-    bcftools-1.18/bcftools view -r ${CHR_NUM} -O v -o ${CHR_VCF37_PRE} ${INPUT_VCF37} 
-    bgzip  -f -c ${CHR_VCF37_PRE} > ${CHR_VCF37_PRE}.gz
-
+    bcftools-1.18/bcftools view -r ${CHR_NUM} -O z -o ${CHR_VCF37_PRE} ${INPUT_VCF37} 
+    
     # liftOver
-    python3 1_prep/lift37to38_for_vep.py -vcf ${CHR_VCF37_PRE}.gz -output ${CHR_VCF38_POST} -chain ${CHAIN} -target chr${CHR_NUM}
+    python3 1_prep/lift37to38_for_vep.py -vcf ${CHR_VCF37_PRE} -output ${CHR_VCF38_POST} -chain ${CHAIN} -target chr${CHR_NUM}
 
     # tidy the data
     python3 1_prep/tidy_chr.py -input_file ${CHR_VCF38_POST} -output_file ${CHR_VCF38_UNSORT} -target chr${CHR_NUM}
 
-    # sort the data 
-    bcftools-1.18/bcftools sort ${CHR_VCF38_UNSORT} > ${CHR_VCF38_SORT}
-
-    # bgzip
-    bgzip -f -c ${CHR_VCF38_SORT} > ${CHR_VCF38}
+    # sort and index the data 
+    bcftools-1.18/bcftools sort ${CHR_VCF38_UNSORT} -O z -o ${CHR_VCF38}
     tabix -p vcf ${CHR_VCF38}
 
 done
