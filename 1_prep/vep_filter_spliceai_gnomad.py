e40cacf7bf0dcb724877adde9aea5c06793e8a92
Yuki Yano <146058945+yuki-yano10@users.noreply.github.com>
2025-11-06 15:46:07 +0900

    Update vep_filter_spliceai_gnomad.py

--- 1_prep/vep_filter_spliceai_gnomad.py
+++ 1_prep/vep_filter_spliceai_gnomad.py
@@ -6,8 +6,10 @@ import sys
 import os
 
 working_directory = sys.argv[1] 
+output_path = f"{working_directory}/post_filter"
+os.makedirs("output_path", exist_ok=True)
 
-output_file = working_directory + "/post_filter/input.all.gnomad001.spliceaiG01.txt"
+output_file = f"{output_path}/input.all.gnomad001.spliceaiG01.txt"
 
 with open(output_file, "w") as hout:
     header = "Mut_key\tChr\tPos\tRef\tAlt\tGene\tSpliceAI_pred_DP_AG\tSpliceAI_pred_DP_AL\tSpliceAI_pred_DP_DG\tSpliceAI_pred_DP_DL\tSpliceAI_pred_DS_AG\tSpliceAI_pred_DS_AL\tSpliceAI_pred_DS_DG\tSpliceAI_pred_DS_DL\tSpliceAI_pred_SYMBOL\tgnomADg_AF\n"
@@ -15,7 +17,7 @@ with open(output_file, "w") as hout:
     
     for i in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,"X"]:
         input_vcf = working_directory + "/post_vep/input."+str(i)+".rare-variant.vep.vcf.gz"
-        output_vcf = working_directory + "/post_filter/input."+ str(i) +".lift38.gnomad001.spliceaiG01.vcf"
+        output_vcf = working_directory + "/post_filter/input."+ str(i) +".gnomad001.spliceaiG01.vcf"
         
         with gzip.open(input_vcf, 'rt') as hin, open(output_vcf,"w") as vout:
            for line in hin:
