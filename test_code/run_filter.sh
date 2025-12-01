d44014f15a585ed88268a1c33da992a47172d999
Yuki Yano <146058945+yuki-yano10@users.noreply.github.com>
2025-12-01 10:05:25 +0900

    Delete test_code directory

--- test_code/run_filter.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#$ -S /usr/bin/bash
-
-WDIR=/home/yano_y/GTEX_validation_project/cros_test
-
-python3 1_prep/vep_filter_spliceai_gnomad.py $WDIR
--- test_code/run_run_define.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#$ -S /bin/bash
-set -euo pipefail
-
-WDIR=/home/yano_y/GTEX_validation_project/cros_test
-GENCODE=/home/yano_y/refdata/wgEncodeGencodeBasicV39.bed.gz
-MANE=/home/yano_y/refdata/MANE/MANE.GRCh38.v1.0.ensembl_genomic.gff.transcript_tag.json
-
-1_prep/run_define.sh ${WDIR} ${GENCODE} ${MANE}
