1619dc7466ac81e1dde127e1b25a4a17450f2d5f
Yuki Yano <146058945+yuki-yano10@users.noreply.github.com>
2026-08-01 15:51:34 +0900

    Update gather_combined_p.py

--- 3_plot/gather_combined_p.py
+++ 3_plot/gather_combined_p.py
@@ -7,21 +7,36 @@ import csv
 import math
 
 def gather_combined_p(input_dir, output_file):
+    fieldnames = [
+        "Key",
+        "CrosSplice_score",
+        "SpliceAI_score",
+    ]
 
     with open(output_file, 'w') as hout:
-        csvwriter = None
+        csvwriter = csv.DictWriter(
+            hout,
+            delimiter = "\t",
+            lineterminator="\n",
+            fieldnames=fieldnames,
+        )
+        csvwriter.writeheader()
+
+        
         for file in glob.glob(input_dir + "/gtex_validation_*_pvalue.tsv"):
             key = file.split('/')[-1].replace("gtex_validation_", "").replace("_pvalue.tsv", "")
             with open(file, 'r') as hin:
                 csvreader = csv.DictReader(hin, delimiter='\t')
-                if csvwriter is None:
-                    csvwriter = csv.DictWriter(hout, delimiter='\t', lineterminator='\n', fieldnames=["Key"] + csvreader.fieldnames)
-                    csvwriter.writeheader()
+
                 for csvobj in csvreader:
                     if csvobj["Tissue"] == "Combined":
-                        csvobj["Key"] = key
-                        print(csvobj)
-                        csvwriter.writerow(csvobj)
+                        csvwriter.writerow({
+                            "Key" : key,
+                            "CrosSplice_score" : csvobj["PV"]
+                            "SpliceAI_score" : csvobj["SpliceAI_score"],
+                        })
+                        break
+
 
 if __name__ == "__main__":
     import sys
